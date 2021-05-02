52b83b52ac96938d762ade09de0ebe0bbb1d5332
lulili <18377335170@163.com>
2021-05-02 10:20:36 +0800

    使用chai.expect添加4个测试用例，第五个不会

---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg class="b-icon" >
+        <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "icon",
+        props:['icon']
+    }
+</script>
+
+<style scoped>
+  .b-icon{
+      width: 1em;
+      height: 1em;
+  }
+</style>
