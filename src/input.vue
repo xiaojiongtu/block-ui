8204cdfba1b909c9d699dd9865c5a0482d59f09c
lulili <18377335170@163.com>
2021-05-03 23:04:12 +0800

    事件

--- src/input.vue
+++ src/input.vue
@@ -1,84 +1,100 @@
 <template>
-   <div class="wrapper">
-     <input :value="value" :class="{error}"  :disabled="disabled" :readonly="readonly" @change="$emit('change',$event)" type="text">
-      <template v-if="error">
-        <icon icon="Shapex" class="icon-error"></icon>
-        <span class="error-message">{{error}}</span>
-      </template>
-   </div>
+  <div class="wrapper">
+    <input :value="value" :class="{error}" :disabled="disabled" :readonly="readonly"
+           @change="$emit('change',$event)"
+           @blur="$emit('blur',$event)"
+           @input="$emit('input',$event)"
+           @focus="$emit('focus',$event)"
+           type="text">
+    <template v-if="error">
+      <icon icon="Shapex" class="icon-error"></icon>
+      <span class="error-message">{{ error }}</span>
+    </template>
+  </div>
 </template>
 
 <script>
 import Icon from "./icon";
+
 export default {
   name: "input",
   components: {Icon},
-  props:{
-    value:{
-      type:String
+  props: {
+    value: {
+      type: String
     },
-    disabled:{
-      type:Boolean,
-      default:false
-    },
-    readonly:{
-      type:Boolean,
+    disabled: {
+      type: Boolean,
       default: false
     },
-    error:{
-      type:String
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
     }
   },
 }
 </script>
 
 <style scoped lang="scss">
-  $border-color:#999;
-  $height:32px;
-  $hover-color:#666;
-  .wrapper{
-    display: flex;
-    align-items: center;
-    >*{
-      margin-right: 0.5em;
-      &:last-child{
-        margin-right: 0;
-      }
-    }
-    > input{
-      height: $height;
-      border: 1px solid $border-color;
-      border-radius: 4px;
-      padding: 0 8px;
-      &:hover{
-        border-color: $hover-color;
-      }
-      &:focus{
-        outline: none;
-        box-shadow:inset 0 1px 3px rgba(0,0,0,.5) ;
-      }
-      &[disabled]{
-        border-color: #999;
-        color: #999;
-        cursor: not-allowed;
-      }
-    &[readonly]{
-      border-color: #999;
-      color: #999;
-     }
-    }
-    .error{
-      >input{
-        border-color: red;
-      }
-    }
-    .icon-error{
-      fill: red;
-      font-size: 18px;
-    }
-    .error-message{
-      color: red;
-      font-size: 14px;
+$border-color: #999;
+$height: 32px;
+$hover-color: #666;
+.wrapper {
+  display: flex;
+  align-items: center;
+
+  > * {
+    margin-right: 0.5em;
+
+    &:last-child {
+      margin-right: 0;
     }
   }
+
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: 4px;
+    padding: 0 8px;
+
+    &:hover {
+      border-color: $hover-color;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: inset 0 1px 3px rgba(0, 0, 0, .5);
+    }
+
+    &[disabled] {
+      border-color: #999;
+      color: #999;
+      cursor: not-allowed;
+    }
+
+    &[readonly] {
+      border-color: #999;
+      color: #999;
+    }
+  }
+
+  .error {
+    > input {
+      border-color: red;
+    }
+  }
+
+  .icon-error {
+    fill: red;
+    font-size: 18px;
+  }
+
+  .error-message {
+    color: red;
+    font-size: 14px;
+  }
+}
 </style>
--- test/input.test.js
+++ test/input.test.js
@@ -15,27 +15,70 @@ describe('bInput', () => {
   it('存在.', () => {
     expect(bInput).to.exist
   })
-  it('should 可以接受value', function () {
-    const Constructor = Vue.extend(bInput)
-    const vm = new Constructor({
-      propsData: {
-        value: '1234'
-      }
-    }).$mount()
-     const elemt=vm.$el.querySelector('input')
-     expect(elemt.value).to.eq('1234')
-      vm.$destroy()
-  });
-  it('should 接受disabled', function () {
-    const Constructor = Vue.extend(bInput)
-    const vm = new Constructor({
-      propsData: {
-        disabled: true
-      }
-    }).$mount()
-    const elemt=vm.$el.querySelector('input')
-    expect(elemt.disabled).to.eq(true)
-    vm.$destroy()
-  });
+   describe('props',()=>{
+     it('should 可以接受value', function () {
+       const Constructor = Vue.extend(bInput)
+       const vm = new Constructor({
+         propsData: {
+           value: '1234'
+         }
+       }).$mount()
+       const elemt=vm.$el.querySelector('input')
+       expect(elemt.value).to.eq('1234')
+       vm.$destroy()
+     });
+     it('should 接受disabled', function () {
+       const Constructor = Vue.extend(bInput)
+       const vm = new Constructor({
+         propsData: {
+           disabled: true
+         }
+       }).$mount()
+       const elemt=vm.$el.querySelector('input')
+       expect(elemt.disabled).to.eq(true)
+       vm.$destroy()
+     });
+     it('should 接受readonly', function () {
+       const Constructor = Vue.extend(bInput)
+       const vm = new Constructor({
+         propsData: {
+           readonly: true
+         }
+       }).$mount()
+       const elemt=vm.$el.querySelector('input')
+       expect(elemt.readOnly).to.eq(true)
+       vm.$destroy()
+     });
+     it('should 接受error', function () {
+       const Constructor = Vue.extend(bInput)
+       const vm = new Constructor({
+         propsData: {
+           error: 'hhhhh'
+         }
+       }).$mount()
+       const useElement = vm.$el.querySelector('use')
+       expect(useElement.getAttribute('xlink:href')).to.equal('#icon-Shapex')
+       const errorMessage=vm.$el.querySelector('.error-message')
+       expect(errorMessage.innerText).to.eq('hhhhh')
+       vm.$destroy()
+     });
+   })
+   describe('事件',()=>{
+     const Constructor = Vue.extend(bInput)
+     let vm;
+     afterEach(()=>{
+       vm.$destroy()
+     })
+     it('支持change事件 ', function () {
+       vm = new Constructor({}).$mount()
+       let spy=chai.spy(function (){})
+       vm.$on('change',spy)
+       let inputEelment=vm.$el.querySelector('input')
+       let event=new Event('change')
+       inputEelment.dispatchEvent(event)
+       expect(spy).to.have.been.called()
+     });
+   })
+
 })
 
