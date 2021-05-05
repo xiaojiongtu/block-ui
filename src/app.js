68599c7986a3197271ac3f07669c7fb77f5a0fce
lulili <18377335170@163.com>
2021-05-05 22:01:57 +0800

    row和col

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import bButton from './b-button.vue'
 import bIcon from './icon'
 import bButtonGroup from './button-group'
 import bInput from './input'
+import Row from  './row'
+import Col from  './col'
 
 Vue.component('b-button',bButton)
 Vue.component('b-icon',bIcon)
 Vue.component('b-button-group',bButtonGroup)
 Vue.component('b-input',bInput)
+Vue.component('b-col',Col)
+Vue.component('b-row',Row)
 new Vue({
     el:'#app',
     data(){
---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "col",
+  props:{
+    span:[String,Number]
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.col{
+  height: 100px;
+  width: 50%;
+  background: antiquewhite;
+  border: solid 1px red;
+
+   $class-prefix:col-;
+    @for $n from 1 through 24{
+      &.#{$class-prefix}#{$n}{
+       width: ($n /24 ) *100%;
+    }
+   }
+}
+</style>
